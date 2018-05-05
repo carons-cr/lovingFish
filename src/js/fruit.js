7ce5523f603460e2b379ab3a5e3cf64c53d222ed
carons-cr <2738794789@qq.com>
2018-05-05 17:56:58 +0800

    commit fruit grow up and move up

--- src/js/fruit.js
+++ src/js/fruit.js
@@ -5,6 +5,8 @@ var Fruit = function() {
     this.bornPositionX = [];
     this.bornPositionY = [];
     this.bornAnemoneIdArr = [];
+    this.width = [];
+    this.speed = [];
 };
 Fruit.prototype.number = 30;
 Fruit.prototype.init = function() {
@@ -15,19 +17,36 @@ Fruit.prototype.init = function() {
         this.alive[i] = true;
         this.bornPositionX[i] = 0;
         this.bornPositionY[i] = 0;
-        this.bornPosition(i);
+        this.born(i);
+        this.width[i] = 0;
+        this.speed[i] = Math.random() * 0.01 + 0.005;
     }
+
 };
 Fruit.prototype.draw = function() {
     for (var i = 0; i < this.number; i++) {
-        canvasContextLower.drawImage(this.orangeFruit, this.bornPositionX[i], this.bornPositionY[i]);
+        if (this.alive[i]) {
+            if (this.width[i] <= 18) {
+                this.width[i] += this.speed[i] * interval;
+            } else {
+                this.bornPositionY[i] -= this.speed[i] * 5 * interval;
+            }
+            canvasContextLower.drawImage(this.orangeFruit, this.bornPositionX[i] - this.width[i] * 0.5,
+                this.bornPositionY[i] - this.width[i] * 0.5, this.width[i], this.width[i]);
+            if (this.bornPositionY[i] < 10) {
+                this.alive[i] = false;
+            }
+        }
     }
 };
-Fruit.prototype.bornPosition = function(i) {
+Fruit.prototype.born = function(i) {
     var bornAnemoneId = this.getBornAnemoneIdNoRepeat();
-    this.bornPositionX[i] = anemone.startPosition[bornAnemoneId] - this.orangeFruit.width/2.0;
-    this.bornPositionY[i] = canvasHeight - anemone.height[bornAnemoneId] - this.orangeFruit.height/2.0;
+    this.bornPositionX[i] = anemone.startPosition[bornAnemoneId];
+    this.bornPositionY[i] = canvasHeight - anemone.height[bornAnemoneId];
+
+    this.width[i] = 0;
 };
+
 
 Fruit.prototype.getBornAnemoneIdNoRepeat = function() {
     var bornAnemoneId = Math.floor(Math.random() * anemone.number);
